2f33baea0836c867480c55248706858546b71533
MatveyKoshkin <113891864+MatveyKoshkin@users.noreply.github.com>
2022-10-21 00:00:25 +0300

    Create header.h

---
+++ header.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cctype>
+#include <cstdlib>
+using namespace std;
+int func(int* p, int count);
+int avarege(int* ar, int count);
